daee1b7ebee8e7366a78fe5d08fba42a5de0e7e7
Kate Malanina <katpm07@gmail.com>
2023-04-11 11:02:53 +0300

    2

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+print("Введите первое число: ")
+a = int(input())
+
+b = int(input('Введите второе число: '))
+
+print(a, ' + ', b, ' = ', a + b)
+"""
+"""
+c = 1
+print(c)
+print(type(c))
+c = bool(c)
+print(c)
+print(type(c))
+"""
+"""
+a = 5.89956
+b = 6.556551
+print(round(a*b, 3))
+"""
+
