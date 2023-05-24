84bee99c2f2f2e94d1b91e2bf744afd7bb511d3e
Kate Malanina <katpm07@gmail.com>
2023-05-24 12:25:07 +0300

    HW7 DONE

---
+++ Lection_funkcii.py
@@ -0,0 +1,33 @@
+# list_1 = [x for x in range(1, 20)]
+# print(list_1)
+# list_1 = list(map(lambda x: x + 10, list_1))
+# print(list_1)
+
+#  .split() - преобразует строку в список
+
+# data = '15 156 96 3 5 8 52 5' 
+# # print(data)
+
+# # data = data.split()
+# # print(data)
+
+# data = list(map(int, data.split()))
+# print(data)
+
+
+# data = [15, 65, 9, 36, 175]
+# res = list(filter(lambda x: x % 10 == 5, data))
+# print(res)
+
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# print(data)
+# res = map(int, data)
+# res = filter(lambda x: x % 2 != 0, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+
+
+
+ 
